9b94c94708b1c423bc41639275d755484f5b3dc4
Arasti <arastigames@gmail.com>
2024-01-21 14:11:06 +0500

    added wave enemy spawning

--- Scripts/Characters/Enemy/EnemiesController.cs
+++ Scripts/Characters/Enemy/EnemiesController.cs
@@ -5,8 +5,15 @@ public partial class EnemiesController : Node
 {
 	public static EnemiesController Instance { get; private set; }
 	
-	[Export] private Vector3 m_SpawnOffset;
+	[Export] private float m_SpawnRange;
+	[Export] private int m_WaveBaseValue = 2;
+	[Export] private float m_EnemySpawnCooldown = 1;
 	[Export] private PackedScene m_EnemyScene;
+
+	private float m_CurrentSpawnCooldown;
+	
+	private int m_WavesCount;
+	private int m_CurrentWaveMaxEnemiesCount;
 
 	private readonly HashSet<EnemyNPC> m_CurrentEnemies = new();
 
@@ -16,14 +23,46 @@ public partial class EnemiesController : Node
 	{
 		base._Ready();
 		Instance = this;
-		SpawnEnemyInstance();
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		if(m_CurrentSpawnCooldown > 0) m_CurrentSpawnCooldown -= (float)delta;
+		
+		if(m_CurrentSpawnCooldown <= 0)
+		{
+			if (m_CurrentEnemies.Count == m_CurrentWaveMaxEnemiesCount)
+			{
+				m_CurrentWaveMaxEnemiesCount = GetWaveEnemyCount(m_WavesCount);
+			}
+			
+			if(m_CurrentEnemies.Count < m_CurrentWaveMaxEnemiesCount)
+			{
+				SpawnEnemyInstance();
+				m_CurrentSpawnCooldown = m_EnemySpawnCooldown;
+				
+				if (m_CurrentEnemies.Count == m_CurrentWaveMaxEnemiesCount)
+				{
+					m_WavesCount++;
+					m_CurrentWaveMaxEnemiesCount = Mathf.RoundToInt(0.3f * m_CurrentWaveMaxEnemiesCount);
+				}				
+			}
+		}
 	}
 	
+	private int GetWaveEnemyCount(int waveIndex)
+	{
+		var waveOffset = waveIndex / 2;
+		return m_WaveBaseValue + m_WaveBaseValue * waveIndex * (1 + GlobalTimer.CurrentTime.Minutes) + GD.RandRange(-waveOffset, waveOffset);
+	}
+
 	private void SpawnEnemyInstance()
 	{
 		var enemy = m_EnemyScene.Instantiate<EnemyNPC>();
 		enemy.HealthSystem.OnDeath += () => m_CurrentEnemies.Remove(enemy);
-		enemy.Position = PlayerGlobalController.Instance.Player.GlobalPosition + m_SpawnOffset;
+		var spawnOffset = RandomExtensions.RandomPointOnUnitCircle() * m_SpawnRange;
+		enemy.Position = PlayerGlobalController.Instance.Player.GlobalPosition + new Vector3(spawnOffset.X, 0, spawnOffset.Y);
 		AddChild(enemy);
 		m_CurrentEnemies.Add(enemy);
 	}
---
+++ Scripts/GlobalTimer.cs
@@ -0,0 +1,16 @@
+using System;
+using Godot;
+
+public partial class GlobalTimer : Node
+{
+	public static TimeSpan CurrentTime { get; private set; }
+	
+	[Export] private Label m_TimerLabel;
+
+	public override void _Process(double delta)
+	{
+		CurrentTime += new TimeSpan(0, 0, 0, 0, (int)(delta * 1000));
+		
+		m_TimerLabel.Text = CurrentTime.ToString("mm':'ss':'ff");
+	}
+}
