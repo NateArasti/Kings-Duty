b685d42e18a97b1ae97d5d191905522dd9c07b8d
Arasti <arastigames@gmail.com>
2024-02-19 21:07:01 +0500

    fixed pause bug

--- Scripts/PauseSystem.cs
+++ Scripts/PauseSystem.cs
@@ -5,7 +5,7 @@ public partial class PauseSystem : Node
 {
 	public static PauseSystem Instance { get; private set; }
 	
-	private static readonly HashSet<Node> m_NodePauseRequests = new();
+	private readonly HashSet<Node> m_NodePauseRequests = new();
 	
 	public bool IsPaused => m_NodePauseRequests.Count > 0;
 
